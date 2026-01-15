01a562e72b45d7471ca71dd015eaaa1541b72c75
llwor94 <me@ldub.io>
2026-01-14 19:40:39 -0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,65 @@
 import Image from "next/image";
-
+import {
+  Heading,
+  Section,
+  Container,
+  Flex,
+  Text,
+  Box,
+  Button,
+  Link,
+} from "@radix-ui/themes";
+import { InstagramLogoIcon } from "@radix-ui/react-icons";
+import CircusTent from "../public/CircusTent.jpg";
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
+    <main>
+      <Image
+        style={{ objectFit: "cover", zIndex: -1, opacity: 0.6 }}
+        src={CircusTent}
+        priority
+        fill
+        alt="Circus Tent"
+      />
+      <Container style={{ position: "relative", padding: "0 10px" }}>
+        <Section pt="5" pb="5">
+          <Heading mb="2" size="9" style={{ fontWeight: "bold" }}>
+            Welcome To Mad Tea(se) Party
+          </Heading>
+        </Section>
+        <Flex justify="center">
+          <Box>
+            <Box>
+              <Text as="p">
+                We are a burning man camp located in Seattle, WA.
+              </Text>
+              <Text as="p">More info coming soon!</Text>
+            </Box>
+            <Flex mt={"3"} direction="column" align="end">
+              <Flex direction="column" align="center">
+                <Button size="4" variant="surface" asChild>
+                  <a
+                    href="https://buytickets.at/madteaseparty/2023652/share/d3759eb9b7da8690e279dfd7e62a296f"
+                    rel={"noreferrer"}
+                    target={"_blank"}
+                  >
+                    Get tickets to our fundraiser
+                  </a>
+                </Button>
+                <Button size="3" variant="ghost" asChild>
+                  <a
+                    href="https://www.instagram.com/mad_tease_party/"
+                    rel={"noreferrer"}
+                    target={"_blank"}
+                  >
+                    <InstagramLogoIcon /> Follow us on instagram
+                  </a>
+                </Button>
+              </Flex>
+            </Flex>
+          </Box>
+        </Flex>
+      </Container>
+    </main>
   );
 }
